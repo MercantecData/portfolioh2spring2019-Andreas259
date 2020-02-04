134886ccd8dd5c69c6403ae962a0d2921760403f
Andreas Madsen <arm@ewe.dk>
2020-02-04 20:59:15 +0100

    Delegates

---
+++ Delegates/Delegates/Math.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    class Math
+    {
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public float Addition(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Addition(string str1, string str2)
+        {
+            int num1 = Int32.Parse(str1);
+            int num2 = Int32.Parse(str2);
+
+            return num1 + num2;
+        }
+
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public float Subtraction(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Subtraction(string str1, string str2)
+        {
+            int num1 = Int32.Parse(str1);
+            int num2 = Int32.Parse(str2);
+
+            return num1 - num2;
+        }
+
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public float Multiplication(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Multiplication(string str1, string str2)
+        {
+            int num1 = Int32.Parse(str1);
+            int num2 = Int32.Parse(str2);
+
+            return num1 * num2;
+        }
+
+        public int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public float Division(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+
+        public int Division(string str1, string str2)
+        {
+            int num1 = Int32.Parse(str1);
+            int num2 = Int32.Parse(str2);
+
+            return num1 / num2;
+        }
+
+        public int SquareRoot(int num)
+        {
+            if (num > 0)
+            {
+                int root = num / 3;
+                int i;
+
+                for (i = 0; i < 32; i++)
+                {
+                    root = (root + num / root) / 2;
+                }
+                return root;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public double SquareRoot(double num)
+        {
+            if (num > 0)
+            {
+                double root = num / 3;
+
+                for (int i = 0; i < 32; i++)
+                {
+                    root = (root + num / root) / 2;
+                }
+
+                return root;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int Exponentiation(int num, int exponent)
+        {
+
+            for (int i = exponent; i > 1; i--)
+            {
+                num *= num;
+            }
+
+            return num;
+        }
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate void method1Delegate();
+        public delegate float method2Delegate();
+        public delegate void method3Delegate(string name, int age, float height);
+        static void Main(string[] args)
+        {
+            // Point delegate1 to Method1
+            method1Delegate delegate1 = Method1;
+            delegate1();
+
+            // Point delegate2 to Method2
+            method2Delegate delegate2 = Method2;
+            Console.WriteLine("Method 2 float value " + delegate2());
+
+            // Point delegate3 to Method3
+            method3Delegate delegate3 = Method3;
+            delegate3("Andreas", 25, 1.82f);
+        }
+        public static void Method1()
+        {
+            Console.WriteLine("Method 1 was invoked");
+        }
+
+        public static float Method2()
+        {
+            return 4.9f;
+        }
+
+        public static void Method3(string name, int age, float height)
+        {
+            Console.WriteLine("Method 3 name is " + name + " and age is " + age + " and height is " + height);
+        }
+    }
+}
