8edd3bfbd86464127984a5a8dc035dbfe6d27be7
Andreas Madsen <arm@ewe.dk>
2020-02-04 09:58:06 +0100

    Dictionaries task 1, 2 & 3

---
+++ Dictionaries_02/Dictionaries_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<float, bool> myDictionary = new Dictionary<float, bool>();
+
+            myDictionary[4.2f] = true;
+            myDictionary[3.2f] = false;
+            myDictionary[2.1f] = true;
+            myDictionary[1.1f] = false;
+            myDictionary[1.7f] = false;
+            myDictionary[7.1f] = true;
+            myDictionary[7.2f] = true;
+            myDictionary[4.3f] = false;
+            myDictionary[4.9f] = false;
+            myDictionary[8.1f] = true;
+        }
+    }
+}
---
+++ Dictionaries_03/Dictionaries_03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<Person, bool> myDictionary = new Dictionary<Person, bool>();
+
+            myDictionary[new Person() { Name = "Andreas" }] = true;
+            myDictionary[new Person() { Age = 19 }] = false;
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
