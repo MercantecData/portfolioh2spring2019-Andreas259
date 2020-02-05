bffbb521335a3c15d53d7f2a68b1b9cda5d2fba4
Andreas Madsen <arm@ewe.dk>
2020-02-05 12:31:35 +0100

    Delegates

--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -9,6 +9,8 @@ namespace Delegates
         public delegate void Method3Delegate(string name, int age, float height);
         public delegate int MathDelegate(int num1, int num2);
         public delegate void MyDelegate();
+        public delegate int IntDelegate(int num1, int num2);
+        public delegate float FloatDelegate(float num1, float num2);
         static void Main(string[] args)
         {
             // Assign type Method1Delegate to variable delegate1 and point it to Method1.
@@ -39,6 +41,18 @@ namespace Delegates
             // Invoke myDelegate, it will call both Method4 and Method5.
             myDelegate();
 
+            // Assign type IntDelegate to variable addition and point it to Lambda Expression
+            IntDelegate addition = (num1, num2) => num1 + num2;
+            Console.WriteLine("Addition 13 + 4 = " + addition(13, 4));
+            // Assign type IntDelegate to variable subtraction and point it to Lambda Expression
+            IntDelegate subtraction = (num1, num2) => num1 - num2;
+            Console.WriteLine("Subtraction 8 - 2 = " + subtraction(8, 2));
+            // Assign type IntDelegate to variable subtraction and point it to Lambda Expression
+            FloatDelegate multiplication = (num1, num2) => num1 * num2;
+            Console.WriteLine("Multiplication 4.2 * 3.1 = " + multiplication(4.2f, 3.1f));
+            // Assign type IntDelegate to variable division and point it to Lambda Expression
+            FloatDelegate division = (num1, num2) => num1 / num2;
+            Console.WriteLine("Multiplication 4 / 32 = " + division(4, 32));
         }
         public static void Method1()
         {
