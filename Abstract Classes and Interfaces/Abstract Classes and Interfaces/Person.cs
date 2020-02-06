ff58fd9d5e29c51c7b7f285bebd5a2e7e1dd2138
Andreas Madsen <arm@ewe.dk>
2020-02-06 13:36:31 +0100

    Abstract Classes and Interfaces

---
+++ Abstract Classes and Interfaces/Abstract Classes and Interfaces/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassesAndInterfaces
+{
+    class Person : IComparable<Person>
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+
+        public int CompareTo(Person item)
+        {
+            // Sort by age [High to low]
+            // return item.age.CompareTo(age);
+            // Sort by age [Low to high]
+            //return age.CompareTo(item.age);
+
+            // Sort by age [Low to high] if names is equal
+            if (item.name == name)
+            {
+                if (item.age == age)
+                {
+                    return gender.CompareTo(item.gender);
+                }
+                return age.CompareTo(item.age);
+            }
+            // Alphabetic sort [A-Z]
+            return name.CompareTo(item.name);
+        }
+    }
+}
--- Abstract Classes and Interfaces/Abstract Classes and Interfaces/Program.cs
+++ Abstract Classes and Interfaces/Abstract Classes and Interfaces/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AbstractClassesAndInterfaces
 {
@@ -18,6 +19,26 @@ namespace AbstractClassesAndInterfaces
             new InterfaceClass1();
             Console.WriteLine("Interface Class 2");
             new InterfaceClass2();
+
+            // Build list and sort it with IComparable
+            var personList = new List<Person>();
+            personList.Add(new Person() { name = "Andreas", age = 25, gender = "Male" });
+            personList.Add(new Person() { name = "Jesper", age = 31, gender = "Male" });
+            personList.Add(new Person() { name = "Louise", age = 11, gender = "Female" });
+            personList.Add(new Person() { name = "Kenneth", age = 25, gender = "Female" });
+            personList.Add(new Person() { name = "Lone", age = 42, gender = "Female" });
+            personList.Add(new Person() { name = "Rasmus", age = 37, gender = "Male" });
+            personList.Add(new Person() { name = "Thomas", age = 29, gender = "Male" });
+            personList.Add(new Person() { name = "Anja", age = 52, gender = "Female" });
+            personList.Add(new Person() { name = "Charlotte", age = 43, gender = "Female" });
+            personList.Add(new Person() { name = "Uffe", age = 51, gender = "Male" });
+
+            personList.Sort();
+
+            foreach (var person in personList)
+            {
+                Console.WriteLine("Name: {0} Age: {1} Gender: {2}", person.name, person.age, person.gender);
+            }
         }
     }
 }
